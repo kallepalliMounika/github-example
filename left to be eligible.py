31a457adb7394c5dcacebd504c4620a759faa598
Kallepalli Mounika <67948348+kallepalliMounika@users.noreply.github.com>
2020-12-04 09:47:59 +0530

    Delete left to be eligible.py

--- left to be eligible.py
+++
@@ -1,8 +0,0 @@
-#write a prgrm to find larger of two numbers
-a=float(input("enter a number:"))
-b=float(input("enter a number:"))
-if (a>b) :
-    print (a, "is big")
-else:
-    print(b,"is big")
-    
