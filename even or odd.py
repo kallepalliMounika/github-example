481a6632e6a7b6a143004293992db4c8ed897d7e
Kallepalli Mounika <67948348+kallepalliMounika@users.noreply.github.com>
2020-12-04 09:45:17 +0530

    Delete even or odd.py

--- even or odd.py
+++
@@ -1,6 +0,0 @@
-#write a prgrm to find the year is leap or not
-year=int(input("enter a year:"))
-if(year%4 and(year%400 or year%100)):
-    print(year,"is not a leap year")
-else:
-    print(year,"is leap year")
