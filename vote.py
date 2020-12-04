90984a6375705138f40375151c6b9e70723f9089
Kallepalli Mounika <67948348+kallepalliMounika@users.noreply.github.com>
2020-12-04 09:50:26 +0530

    Delete vote.py

--- vote.py
+++
@@ -1,6 +0,0 @@
-#write a prgrm to determine whether a person is eligible to vote
-age=int(input("enter age:"))
-if (age>=18):
-    print("person is eligible to vote")
-else:
-    print("person is not eligible to vote")
