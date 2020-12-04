b02800211c5e135e9b00a438c0bd839628d5a0d7
Kallepalli Mounika <67948348+kallepalliMounika@users.noreply.github.com>
2020-12-04 09:49:19 +0530

    Delete simple calc.py

--- simple calc.py
+++
@@ -1,8 +0,0 @@
-def isequal(x,y):
-    if x==y:
-        print(x,"and",y,"are equal")
-    else:
-        print(x,"and",y,"are not equal")
-n=int(input("enter a number"))
-c=int(input("enter a number"))
-isequal(n,c)
