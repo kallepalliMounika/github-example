c1061c05344b107a0d88fdc00cdc364214a1d5e1
Kallepalli Mounika <67948348+kallepalliMounika@users.noreply.github.com>
2020-12-04 09:49:48 +0530

    Delete sum of cubes.py

--- sum of cubes.py
+++
@@ -1,9 +0,0 @@
-def s_cal(a,b,c):
-    if c=='add':
-        print(a+b)
-    elif c=='subtract':
-        print(a-b)
-    elif c=='multiply':
-        print(a*b)
-    elif c=='division':
-        print(a/b)
